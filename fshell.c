c1201fc9c36b78e7a2701507e32fea0352c78b92
Miller <millera16@debian-9.3.0-template>
2018-05-02 19:45:55 -0700

    Added fshell.c, the source code for interpreting string input from the user and executing the proper program. Currently takes user input and recognizes exit.

--- fshell.c
+++ fshell.c
@@ -2,6 +2,7 @@
 #include<stdlib.h>
 #include<errno.h>
 #include<unistd.h>
+#include<string.h>
 
 //fshell.c
 //This is the actual shell interpreter called by falsh.c
@@ -11,20 +12,35 @@
 
 
 
-void help();
-
-void exit();
 
 
 int main(int argc, char *argv[])
 {
 	char cwd[1024];
+	char *line;
+	size_t n = 0;
+	size_t input;
+	char str[7] = "exit\n";
 
+
+	//Gets current working directory
 	if (getcwd(cwd, sizeof(cwd)) != NULL)
-		fprintf(stdout, "%s\n" , cwd);
-	else
+		fprintf(stdout, "starting falcon shell. CWD is %s\n", cwd);
+	else //Fail condition for getcwd failure
 		perror("current working directory error\n");
+	
 
+	//Loop that gets user strings to deconstruct them into commands
+	//exits whenever the string input is exit
+	while (strcmp(line, str) != 0)
+	{	
+		//Display current directory before taking a commmand
+		fprintf(stdout, "%s$ ", cwd);
+		//Get input from stdin
+		input = getline(&line, &n, stdin);
+
+		
+	}
 
 	return 0;
 }
